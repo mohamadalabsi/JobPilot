d3eefaa3b33d6ca413c611326c6ddc76c50f0101
mohamadalabsi <absimoda1@gmail.com>
2026-03-18 00:46:57 +0100

    finishing signing in

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -6,10 +6,55 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signIn } from "@/lib/auth/auth-client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Signin() {
 
+
+   const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+  //  if there is an error we will display it and if the form is submitting we will disable the button and show a loading state
+   const [error, setError] = useState("");
+   const [loading, setLoading] = useState(false);
+  
+   const router = useRouter(); // we will use this to redirect the user to the dashboard page after they sign up successfully
+  
+  //   now function will run whenever the user submits the form // it is async because we will be making an api call to sign up the user and we want to wait for that to finish before we do anything else
+  async function handleSubmit(e: React.FormEvent) { 
+    // it will be called from the form 
+      e.preventDefault();
+      setLoading(true); // set loading to true when the form is submitted and we will set it to false when the form is done submitting or if there is an error
+      setError(""); // reset the error state before submitting the form
+  
+      // ! now we sigin up the user in try 
+      try {
+        // we need to write something that help us with all the operations related to authentication like signing up , signing in , see auth-client.ts for that
+        // we use email and password authentication , so it is the function that gonna sign up the user using that 
+        const result = await signIn.email({
+            email,
+            password
+          });
+  
+          if (result.error) {
+            setError(result.error.message ?? "An error occurred while signing up"); // set the error state if there is an error
+          }else {
+          //  if it was successful we will redirect the user to the dashboard page and we can do that using the useRouter hook from next/navigation
+            router.push("/dashboard");
+          }
+  
+      } catch (err) {
+          setError("An unexpected error occurred"); // set the error state if there is an error
+      } finally {
+        // no matter what happens we will set loading to false when the form is done submitting
+          setLoading(false); 
+      }
+  
+  }
+  
   return (
     // min-h-[calc(100vh-4rem)] is min height minus the height of the header (4rem) but we have to calculate it
     <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
@@ -23,8 +68,14 @@ export default function Signin() {
            Enter your email and password to access your job application tracker
           </CardDescription>
         </CardHeader>
-        <form  className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
           <CardContent className="space-y-4">
+            {error && (
+              <div className="rounded-md bg-destructive/15 p-3 text-sm text-destructive">
+               {error}
+              </div>
+            )}
+         
          
             <div className="space-y-2">
               <Label htmlFor="email" className="text-gray-700">
@@ -34,6 +85,7 @@ export default function Signin() {
                 id="email"
                 type="email"
                 placeholder="you@example.com"
+                onChange={(e)=>setEmail(e.target.value)}
                 
                 required
                 className="border-gray-300 focus:border-primary focus:ring-primary"
@@ -47,6 +99,7 @@ export default function Signin() {
                 id="password"
                 type="password"
                 placeholder="********"
+                onChange={(e)=>setPassword(e.target.value)}
                
                 required
                 minLength={8}
@@ -58,15 +111,17 @@ export default function Signin() {
             <Button
               type="submit"
               className="w-full bg-primary hover:bg-primary/90"
+              disabled={loading} // disable the button when the form is submitting
                
             >
-              Sign Up
+             {loading ? "Signing In..." : "Sign In"}
             </Button>
             <p className="text-center text-sm text-gray-600">
               don't have an account?{" "}
               <Link
                 href="/sign-up"
                 className="font-medium text-primary hover:underline"
+                
               >
                 Sign up
               </Link>
